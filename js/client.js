f53608d730badb330172981f1277d0db99b8a8c6
byDeinon <bydeinon@gmail.com>
2016-03-13 14:21:13 +0000

    Fix script and template

--- js/client.js
+++ js/client.js
@@ -1,31 +1,29 @@
 'use strict';
 
 angular.module('flixrch', [])
-.controller('mainCtrl', function($scope, $http){
-  $scope.$watch('query', function(){
-    fetch();
-  });
-  
-  $scope.query = "Ran";
-  
-  function fetch(){
-//     Get JSON movie data
-    $http.get("https://www.omdbapi.com/?t=" + $scope.query + "&tomatoes=true&plot=full&r=json")
+  .controller('mainCtrl', function($scope, $http) {
+    $scope.$watch('query', function() {
+      fetch();
+    });
+
+    $scope.query = "Ran";
+
+    function fetch() {
+      $http.get("https://www.omdbapi.com/?t=" + $scope.query + "&tomatoes=true&plot=full&r=json")
         .then(function(response) {
           $scope.movie = response.data;
         });
-//     Get related movies
-    $http.get("https://www.omdbapi.com/?s=" + $scope.search)
+      $http.get("https://www.omdbapi.com/?s=" + $scope.query)
         .then(function(response) {
           $scope.related = response.data;
         });
-  }
-  
-      $scope.update = function(movie) {
-      $scope.search = movie.Title;
+    }
+
+    $scope.update = function(movie) {
+      $scope.query = movie.Title;
     };
 
     $scope.select = function() {
       this.setSelectionRange(0, this.value.length);
     };
-});
+  });
